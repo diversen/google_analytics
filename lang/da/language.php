31c7f626fee756860b40e004714991e82fa59a05
Dennis Iversen <dennis.iversen@gmail.com>
2016-02-17 16:53:34 +0100

    lang

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,6 +2,6 @@
 
 $LANG = array();
 
-$LANG['Google analytics'] = 'Google Analytics';
 $LANG['Google analytics code'] = 'Google analytics kode';
 $LANG['Google webmaster code'] = 'Google webmaster kode';
+$LANG['Google analytics'] = 'Google Analytics';
