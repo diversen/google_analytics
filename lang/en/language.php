ad3a16e77708e954215f4b4bddbc124ba0cfad14
Dennis Iversen <dennis.iversen@gmail.com>
2016-02-29 13:40:45 +0100

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -2,10 +2,10 @@
 
 $LANG = array();
 
-// Translation of file modules/analytics/install.inc
-
-$LANG['Google analytics'] = 'Google analytics';
 // Translation of file modules/analytics/config/module.php
 
 $LANG['Google analytics code'] = 'Google analytics code';
 $LANG['Google webmaster code'] = 'Google webmaster code';
+// Translation of file modules/analytics/install.inc
+
+$LANG['Google analytics'] = 'Google analytics';
